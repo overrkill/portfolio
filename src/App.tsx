2da262711ac6d70f97cbb9f4b5f25a4beb17bc5a
Abhishek Kale <zabhishekkale@gmail.com>
2020-07-21 14:01:08 +0530

    deployment additions

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Abhishek Kale
+      </header>
+
+      <div className="content">
+        <h3>
+          Abhishek
+          Kale
+       </h3>
+        <p>
+          Creative Coder <br />
+         Developer <br />
+        </p>
+      </div>
+
+      <footer>
+        site is under development
+      </footer>
+    </div>
+  );
+}
+
+export default App;
